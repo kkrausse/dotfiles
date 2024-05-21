fbab6fdc5cb18279d5fc3a057109b1de7c4690a5
kevin.krausse <kevin.krausse@taxbit.com>
2024-05-21 14:05:55 -0700

    add cache to clean up subnode shit

--- doom.d/init.el
+++ doom.d/init.el
@@ -55,7 +55,7 @@
        ;;window-select     ; visually switch windows
 ;;       workspaces        ; tab emulation, persistence & separate wor
 ;;note
-;;       zen               ; distraction-free coding or writing
+      zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
